bfb94913679d0029010a9cb8b6dd1132ccffa7b9
Bill Brubaker <brubaker.bill@gmail.com>
2019-04-04 10:44:21 -0700

    consolidated setup process

--- setup.py
+++ setup.py
@@ -14,3 +14,23 @@
 # 	print(message)
 
 # usage(use_message)
+
+# Import Python libraries
+
+# Import D.O.T. libraries
+# networking
+# configuring
+# Hosting
+
+
+# TODO get current OS
+# TODO check for git
+# TODO check for automation tools
+# TODO check for configuration mangement tools
+# TODO check if virtual
+# TODO check network namp 
+# TODO classify nodes
+# TODO classify databases
+# TODO classify webservers
+# TODO find local users
+# TODO find node users
